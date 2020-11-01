5d92c6902048020f8d80eb7d696e7091396ad31b
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2020-11-01 17:27:02 +0100

    Add GatsbyJS plugins for Netlify hosting

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,8 @@
 module.exports = {
   /* Your site config here */
   plugins: [
+    'gatsby-plugin-netlify',
+    'gatsby-plugin-netlify-cache',
     {
       resolve: 'gatsby-theme-material-ui',
       options: {
