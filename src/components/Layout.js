3f102c010cf9332fdd2a5ce51fb4af186318d981
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2020-11-01 13:39:23 +0100

    Add a top navigation

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,19 +4,50 @@ import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
 
 import { makeStyles } from '@material-ui/core/styles';
-import { Container } from '@material-ui/core';
+import { Box, Container, Link as MLink } from '@material-ui/core';
+import { Link } from 'gatsby';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
     minHeight: '100vh',
   },
+  nav: {
+    display: 'flex',
+    justifyContent: 'center',
+    padding: theme.spacing(2, 0),
+  },
+  navItem: {
+    margin: theme.spacing(0, 2),
+  },
+  currentNavItem: {
+    color: theme.palette.secondary.main,
+  },
   main: {
     marginTop: '2rem',
     marginBottom: '2rem',
   },
-});
+}));
+
+const menu = [
+  {
+    children: 'Formulaire › Bookmarklet',
+    to: '/',
+  },
+  {
+    children: 'Bookmarklet › Bookmarklet',
+    to: '/simple',
+  },
+  {
+    children: 'Alternatives',
+    to: '/alternatives',
+  },
+  {
+    children: 'Github',
+    to: 'https://github.com/mabhub/bookmarklet-attestation-deplacement',
+  },
+];
 
 const Layout = ({ className, title = '', ...rest }) => {
   const classes = useStyles();
@@ -28,6 +59,21 @@ const Layout = ({ className, title = '', ...rest }) => {
         title={title}
         titleTemplate="%s | Aide au remplissage de formulaire"
       />
+
+      <Container>
+        <Box className={classes.nav}>
+          {menu.map(props => (
+            <Box key={props.to} className={classes.navItem}>
+              <MLink
+                activeClassName={classes.currentNavItem}
+                component={Link}
+                {...props}
+              />
+            </Box>
+          ))}
+        </Box>
+      </Container>
+
       <Container
         component="main"
         className={clsx(classes.main, className)}
